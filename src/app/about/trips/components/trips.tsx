5e1a70d3a1dc5ad0166fdd184b55d4c26e586eae
Balula12 <gugabasigb2@gmail.com>
2023-07-04 20:50:15 -0300

    initial commit

---
+++ src/app/about/trips/components/trips.tsx
@@ -0,0 +1,21 @@
+
+import React, { useState } from "react";
+import { prisma } from "../../../lib/pisma";
+
+const getTrips = async () => {
+    const trips = await prisma.trip.findMany({})
+    
+    return trips;
+}
+
+const Trips = async () => {
+    const data = await fetch("http://jsonplaceholder.typicode.com.posts",{
+       
+     }).then((res) => res.json())
+
+    
+    return (<div>{data.map((i: any) => <p key={i. id}>{i.title}</p>)}</div>);
+};
+
+export default Trips;
+
